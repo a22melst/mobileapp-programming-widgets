b0f7dcab034716e49562f21d984de495eb4651cb
Melker Stafverfeldt <a22melst@student.his.se>
2023-04-12 13:45:26 +0200

    Profilbild och namn tillagt. Egen bildfil använd.

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.widgets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Profilbild
+    ImageView imageView;
+    Drawable drawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Profilbild
+        imageView = findViewById(R.id.imageView);
+        drawable = getResources().getDrawable(R.drawable.profilepic);
+        imageView.setImageDrawable(drawable);
     }
 }
